404c51d3a3f3e3378a2f4ec7969adcdc205db28f
mulunshiyi641 <mulunshiyi641@hotmail.com>
2017-04-25 15:11:19 -0500

    Add files via upload

---
+++ name.cpp
@@ -0,0 +1,38 @@
+/*
+ * name.cpp
+ *
+ *  Created on: Apr 25, 2017
+ *      Author: shiyi
+ */
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int nameScore = 0;
+	int totalNameScore = 0;
+	int counter = 0;
+	int tempScore = 0;
+
+	std::ifstream infile("names.txt");
+	std::string line;
+	string word;
+
+	{
+		for(int i = 0; i < word.length; i++)
+
+		int _char = word[i];
+		tempScore += int_char;
+
+			cout<<"For "<<name<<" the score is: "<<nameScore<<endl;
+			totalNameScore += nameScore;
+			counter++;
+}
+	cout<<"Total score for all names is : "<<totalNameScore<<endl;
+
+	return 0;
+}
