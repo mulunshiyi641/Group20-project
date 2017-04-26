6ab8d3b2d6db1e4a5976a53ee926d3abdb521997
graemefink <finkgraeme536926@gmail.com>
2017-04-26 16:05:27 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+
+using namespace std;
+
+
+int const ARRAYSIZE = 5163;
+
+int main()
+{
+	int a;
+	
+	ifstream file("names.txt");
+	
+	string myArray[5163];
+
+	//read file into array
+	if (file.is_open())
+	{
+
+		for (int i = 0; i < 5163; ++i)
+		{
+			file >> myArray[i];
+		}
+	}
+	
+	//sort names alphabetically
+	int z = sizeof(myArray) / sizeof(myArray[0]); 
+
+	sort(myArray, myArray + z); 
+
+	for (int y = 0; y < z; y++) {
+		cout << myArray[y] << endl;
+	}
+
+
+	return 0;
+
+	cin >> a;
+
+}
