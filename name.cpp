519a36242d9754097c9e59cbfde353fa48ebc32b
mulunshiyi641 <mulunshiyi641@hotmail.com>
2017-05-03 22:34:04 -0500

    Update name.cpp

--- name.cpp
+++ name.cpp
@@ -6,37 +6,39 @@
 using namespace std;
 int main()
 {
-int i = 0;
-ifstream fin;
-string temp = "";
-string text = "";
-int const arraySize = 7163;
-int finalNameScore = 0;
-int totalScore = 0;
-string myArray[7163];
-fin.open("names.txt");
-int count = 1;
-while (!fin.eof())
-{
-getline(fin, temp);
-myArray[i] = temp;
-i++;
-}
-cout << text << endl;
-fin.close();
-for (int i = 0; i < 5163; ++i)
-{
-string aName = myArray[i];
-{
-int total = 0;
-for (int i = 0; i < aName.size(); i++)
-{
-total = total + (aName[i] - 64);
-}
+  int i = 0;
+  ifstream fin;
+  string temp = "";
+  string text = "";
+  int const arraySize = 7163;
+  int finalNameScore = 0;
+  int totalScore = 0;
+  string myArray[7163];
+  
+  fin.open("names.txt");
+  int count = 1;
+  while (!fin.eof())
+  {
+    getline(fin, temp);
+    myArray[i] = temp;
+    i++;
+  }
+  cout << text << endl;
+  fin.close();
+  
+  for (int i = 0; i < 5163; ++i)
+  {
+    string aName = myArray[i];
+    {
+      int total = 0;
+      for (int i = 0; i < aName.size(); i++)
+      {
+       total = total + (aName[i] - 64);
+      }
 
-}
+    }
+  }
 system("pause");
 return 0 ;
-}
 }
 
